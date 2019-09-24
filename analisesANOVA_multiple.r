b59671237e817364359fa76339aab9729b666af6
solkan1201 <solkan1201@gmail.com>
2019-09-24 14:14:07 -0300

    update

--- analisesANOVA_multiple.r
+++ analisesANOVA_multiple.r
@@ -1,1 +1,26 @@
  
+#install.packages("ggpubr")
+#install.packages('dplyr')
+library("ggpubr")
+library("dplyr")
+mydado = read.csv('/home/superusuario/Documents/sheila/dados.csv', sep=';')
+
+summary(mydado)
+
+# Show the levels
+levels(mydado$inst)
+levels(mydado$forca)
+levels(mydado$med)
+boxplot(mydado$queda ~ mydado$inst)
+boxplot(mydado$forca ~ mydado$inst)
+aov.mydado <- aov(mydado$queda ~ mydado$inst + mydado$forca + mydado$med + mydado$inst:mydado$forca + mydado$inst:mydado$med + mydado$forca:mydado$med)
+summary(aov.mydado)
+
+aov1.mydado <- aov(mydado$forca ~ mydado$inst + mydado$queda + mydado$med + mydado$inst:mydado$forca + mydado$inst:mydado$queda + mydado$forca:mydado$med)
+summary(aov1.mydado)
+
+TukeyHSD(aov.mydado)
+kruskal.test(mydado$queda ~ mydado$inst , data=mydado)
+kruskal.test(mydado$forca ~ mydado$inst , data=mydado)
+kruskal.test(mydado$forca ~ mydado$queda , data=mydado)
+kruskal.test(mydado$forca ~ mydado$med , data=mydado)
